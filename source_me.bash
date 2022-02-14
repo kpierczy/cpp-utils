e9933d1afb289fbcf490090af11e655638b9004d
Cris <krzysztof.pierczyk@gmail.com>
2022-02-14 22:15:34 +0100

    bash-utils update

--- source_me.bash
+++ source_me.bash
@@ -2,7 +2,7 @@
 # @file     source_me.bash
 # @author   Krzysztof Pierczyk (krzysztof.pierczyk@gmail.com)
 # @date     Thursday, 3rd February 2022 1:51:57 pm
-# @modified Thursday, 3rd February 2022 9:42:03 pm
+# @modified Monday, 14th February 2022 3:27:44 pm
 # @project  mbed-utils
 # @brief
 #    
@@ -12,16 +12,14 @@
 # ====================================================================================================================================
 
 # Absolute path to the project's root directory
-if [[ ! -n ${CPP_UTILS_HOME+x} ]]; then
-    export CPP_UTILS_HOME=~/Desktop/cpp-utils
-fi
+export CPP_UTILS_HOME="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 # ====================================================== Install dependencies ====================================================== #
 
 # Update subprojects
 git submodule update --init --recursive
 
-# Source bsah-helper library
+# Source bash-helper library
 source $CPP_UTILS_HOME/extern/bash-utils/source_me.bash
 
 # Install dependencies
